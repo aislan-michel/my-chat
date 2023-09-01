278a05c370459d670af9faeed94285f07d401e59
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2023-09-01 15:44:08 -0300

    create project

---
+++ src/MyChat.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyChat.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
